bcc42c90692e966c0296f1ded6abc6b847c0fb4a
Cristhian Cuenca <ccuenca24@gmail.com>
2024-05-30 07:38:46 -0500

    local example with ollama added

--- starter-local.py
+++ starter-local.py
@@ -2,23 +2,31 @@ import logging
 import sys
 import os.path
 
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.ollama import Ollama
+
 from llama_index.core import (
     VectorStoreIndex,
     SimpleDirectoryReader,
     StorageContext,
     load_index_from_storage,
+    Settings
 )
 
-# logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-# logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+# bge-base embedding model
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+
+# ollama
+Settings.llm = Ollama(model="llama3", request_timeout=360.0)
 
 # check if storage already exists
-PERSIST_DIR = "./storage-starter"
+PERSIST_DIR = "./storage"
 
 if not os.path.exists(PERSIST_DIR):
     # load the documents and create the index
     documents = SimpleDirectoryReader("data").load_data()
     index = VectorStoreIndex.from_documents(documents)
+
     # store it for later
     index.storage_context.persist(persist_dir=PERSIST_DIR)
 else:
@@ -26,8 +34,7 @@ else:
     storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
     index = load_index_from_storage(storage_context)
 
-# Either way we can now query the index
-query_engine = index.as_query_engine()
-response = query_engine.query("Cual es el objetivo de este documento?")
 
+query_engine = index.as_query_engine()
+response = query_engine.query("Que busca el autor con esta información?")
 print(response)
